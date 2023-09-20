43225d37ad570b29ec9ae9a4bb9cd48622667573
Kehinde Omokungbe Kardozo <kehindefranklin@hmail.com>
2023-09-20 16:35:22 +0100

    Running node app

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var ejs = require('ejs')
+
+
+var app = express();
+
+app.listen(8080);
+
+
+
+app.get('/',function(req,res){
+
+    res.send("Hello Mr. Kehinde");
+
+});
